4a5345c1722aac6484de558efa2654d6f21833e3
Robert Clark <37342772+MontyKhan@users.noreply.github.com>
2020-03-10 15:46:08 +0000

    Issue #1 - Fixed formatting

--- Threshhold_CTs.m
+++ Threshhold_CTs.m
@@ -1,52 +1,54 @@
-close all;
+% Threshold_CTs.m
+% Description: Loads DICOM files from an absolute address in the expected
+%              format, and generates a set of binary masks and a 3D point 
+%              cloud representing each mask as a layer.
 
+close all;                                                      % Close open windows and figures to prevent clutter.
+
+% Initialize vectors
 imageVector = [];
-
 mask_3d = [];
 
-% for n=1:466
-% for n=1:47
+% Set paths
+input = 'D:\Final Year Project\Patient Elbow CTs\00000000_DICOM_CT_2018_02_28_000';
+output = 'D:\Final Year Project\threshholds3';
+
 % Left joint
 % for n = 1:92
 % Right joint
+
 for n = 1:90
-%   Read image from file
-    filenum = 6624043 + n;
-%     filenum = 6623828;
-% Left joint
-    % filename = strcat('D:\Final Year Project\Patient Elbow CTs\00000000_DICOM_CT_2018_02_28_000',num2str(filenum),'.dcm'); 
-% Right joint
-    filename = strcat('D:\Final Year Project\Patient Elbow CTs\00000000_DICOM_CT_2018_02_28_000',num2str(filenum),'.dcm'); 
+    %% Generate masks
+    filenum = 6624043 + n;                                      % Read image from file (right joint)
+%   filenum = 6623828;                                          % Read image from file (left joint)
+
+    filename = strcat(input ...                                 % Load file
+                      ,num2str(filenum) ...
+                      ,'.dcm');
     info = dicominfo(filename);
-    image = dicomread(info);            % Will be in Grey units.
+    image = dicomread(info);                                    % Will be in Grey units.
     
-%     Display raw image
-%     figure
-%     imshow(image,[]);
+    mask = (image > 300);                                       % Threshold out values below 300 (i.e. soft tissue)
+    seOpen = strel('disk',2);                                   % Set dilate
+    seClose = strel('disk',2);                                  % Set erode
+    cleanMask = imopen(mask, seOpen);                           % Dilate
+    cleanMask = imclose(cleanMask,seClose);                     % Erode
     
-    mask = (image > 300);
-    seOpen = strel('disk',2);
-    seClose = strel('disk',2);
-    cleanMask = imopen(mask, seOpen);
-    cleanMask = imclose(cleanMask,seClose);
-    
-    imageDisplay = mat2gray(image);
-    
-%     figure
-%     imshow(mask, []);
+    %% IF LOOKING TO COMPARE IMAGES
+    % imageDisplay = mat2gray(image);                           % Convert original image to grayscale
+    % outdisplay = montage({imageDisplay mask cleanMask}  ...   % Create new image of original image, mask and cleaned mask in a row
+    %                      , 'Size', [1 3] ...              
+    %                      ,'DisplayRange',[]);
+    % cd 'D:\Final Year Project\threshholds2';                  % Change working directory             
+    % filename = strcat('th_',num2str(n),'.png');               % Save
+    % saveas(outdisplay,filename);
 
-      %outdisplay = imshowpair(image,mask,'montage');
-      %imshowpair(mask,cleanMask,'montage');
-%       outdisplay = montage({imageDisplay mask cleanMask}, 'Size', [1 3], 'DisplayRange',[]);
-%       cd 'D:\Final Year Project\threshholds2';
-%       filename = strcat('th_',num2str(n),'.png');
-%       saveas(outdisplay,filename);
-
-%       figure;
-%       imshow(cleanMask);
-%       
+    %% IF LOOKING TO SAVE MASKS
+    cd 'D:\Final Year Project\threshholds3';
+    filename = strcat('th_',num2str(n),'.png');
+    imwrite(cleanMask,filename);
       
-    % Convert to list of coordinates
+    %% Convert to list of coordinates
     for i = 1:size(cleanMask,2)
         for j = 1:size(cleanMask,1)
             if (cleanMask(i,j) == 1)
@@ -54,14 +56,10 @@ for n = 1:90
             end
         end
     end
-
-%     mask_3d = cat(3, mask_3d, cleanMask);
 end
-
-% scatter3(mask_3d(:,2), mask_3d(:,1), mask_3d(:,3));
-% blockPlot(mask_3d);
 
 % Create point cloud
 ptCloud = pointCloud(mask_3d);
 
+% Show point cloud
 pcshow(ptCloud)
--- icp_time.m
+++ icp_time.m
@@ -1,21 +1,25 @@
+% icp_time.m
+% Description: Used to compare accuracy of different downsampling levels
+
+% Set original rotation matrix
 rotate = [cos(pi/4) -sin(pi/4) 0 0;
           sin(pi/4)  cos(pi/4) 0 0;
           0          0         1 0;
           0          0         0 1];
       
-transform = affine3d(rotate);
+transform = affine3d(rotate);                                           % Create affine transformation from rotation matrix
 
-tElbow = pctransform(lElbow, transform);
+tElbow = pctransform(lElbow, transform);                                % Transform point cloud
 
-step = 10
+step = 10                                                               % Set downsampling level to 10, publish to terminal
 
 tic
 
-ds_l = pcdownsample(lElbow, 'gridAverage', step);
-ds_t = pcdownsample(tElbow, 'gridAverage', step);
-[tform,movingreg,rmse] = pcregistericp(ds_l,ds_t,'Extrapolate',true);
+ds_l = pcdownsample(lElbow, 'gridAverage', step);                       % Downsample original limb by step
+ds_t = pcdownsample(tElbow, 'gridAverage', step);                       % Downsample rotated limb by step
+[tform,movingreg,rmse] = pcregistericp(ds_l,ds_t,'Extrapolate',true);   % Extrapolate rotation matrix
 
-toc
+toc                                                                     % Publish time taken to terminal
 
 step = 5
 
